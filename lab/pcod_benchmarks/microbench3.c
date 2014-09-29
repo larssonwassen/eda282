0ebd69ae91dae4995f1c4d564f3f91d0d5ac8a21
Dan Larsson <dan.jv.larsson@gmail.com>
2014-09-29 21:06:24 +0200

    fu

---
+++ lab/pcod_benchmarks/microbench3.c
@@ -0,0 +1,75 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define min(a,b) (((a)<(b))?(a):(b))
+#define CSIZE 16
+
+static pthread_mutex_t lock;
+static pthread_barrier_t barrier;
+
+int* counterArray;
+
+int nthreads;
+int ncounters;
+
+void* chain(void* arglist)
+{
+    int i, j;
+
+    for (i = 0; i < nthreads; i++) { 
+        pthread_mutex_lock(&lock);
+
+        for (j = 0; j < ncounters; j++) 
+            counterArray[j*CSIZE]++;
+       
+        pthread_mutex_unlock(&lock);
+        pthread_barrier_wait(&barrier);
+    }
+        
+    return NULL;
+}
+
+
+int main(int argc, const char** const argv)
+{
+    if (argc != 3) {
+       printf("Usage: ./microbench2 <nthreads> <ncounters>\n");
+       exit(1);
+    }
+    
+    nthreads = atoi(argv[1]);
+    ncounters = atoi(argv[2]);
+
+    if (nthreads < 2 || ncounters < 1) {
+        printf("This test requires at least 2 CPUs, 1 counter variable\n");
+        exit(1);
+    }
+    
+    int i;
+    pthread_t pth[nthreads];
+    pthread_attr_t attr;
+    
+    pthread_attr_init(&attr);
+    pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+    
+    pthread_mutex_init(&lock, NULL);
+    printf("Init done\n");
+    
+    
+    pthread_barrier_init(&barrier, NULL, nthreads);
+    
+    counterArray = (int*) calloc(ncounters*CSIZE, sizeof(int));
+    
+    for (i = 0; i < nthreads; i++) {
+        pthread_create(&pth[i], &attr, chain, NULL);
+    }
+    
+    for (i = 0; i < nthreads; i++) { 
+        pthread_join(pth[i], NULL);
+    }
+    
+    printf("Val: %d\n", counterArray[0]);
+    printf("PASSED :-)\n");
+    return 0;
+}
